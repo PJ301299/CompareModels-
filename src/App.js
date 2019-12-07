710848a9a476f66cf49e3d09a8a34da3e35e72bf
PJ301299 <pruthav.j@gmail.com>
2019-12-07 17:02:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import i10 from './i10.jpg';
+import i10r from './i10r.jpg';
+import i11 from './i11.jpg';
+import i11pro from './i11pro.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  render()
+  {
+    return(
+      <div>
+        <h1 className="App-header">Compare Models</h1>
+        <div className="row">
+
+          <div className="col-sm-3" style={{backgroundColor:"white", borderStyle:"solid", borderColor:"black"}}>
+              <img src ={i10} className="image" style={{width:"100%", height:"400px"}}/>
+                <div className="caption">
+                <p>Model</p>
+                <p>Price</p>
+                </div>
+            </div>
+
+          <div className="col-sm-3" style={{backgroundColor:"white",  borderStyle:"solid", borderColor:"black"}}>
+            <img src ={i10r} className="image" style={{width:"100%", height:"400px",}}/>
+            <div className="caption">
+            <p>Model</p>
+            <p>Price</p>
+            </div>
+          </div>
+
+          <div className="col-sm-3" style={{backgroundColor:"white", borderStyle:"solid", borderColor:"black"}}>
+            <img src ={i11} className="image" style={{width:"100%", height:"400px"}}/>
+            <div className="caption">
+            <p>Model</p>
+            <p>Price</p>
+            </div>
+          </div>
+
+          <div className="col-sm-3" style={{backgroundColor:"white", borderStyle:"solid", borderColor:"black"}}>
+            <img src ={i11pro} style={{width:"100%", height:"400px"}}/>
+            <div className="caption">
+            <p>Model</p>
+            <p>Price</p>
+            </div>
+          </div>
+        </div>
+        </div>
+    )
+  }
+
 }
 
 export default App;
