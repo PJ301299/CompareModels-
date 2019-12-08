bf4310511d0f363189eff4b5a42c6eb7d346639c
PJ301299 <pruthav.j@gmail.com>
2019-12-08 11:46:04 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,76 @@ import i11pro from './i11pro.jpg';
 
 
 class App extends React.Component{
+
+
+  constructor(props){
+    super(props);
+    this.state = {
+      data:[
+            {
+              id : 1,
+              model : "iPhone X",
+              price : "$899"
+            },
+            {
+              id : 2,
+              model : "iPhone Xr",
+              price : "$599",
+            },
+            {
+              id : 3,
+              model : "iPhone 11",
+              price : "$699",
+            },
+            {
+              id : 4,
+              model : "iPhone 11 pro",
+              price : "$999",
+            }
+          ],
+      list:[]
+    }
+  }
+
+
+  addItem(x)
+  {
+    var temp = [...this.state.data];
+    const itemArr = temp.filter(mobile => mobile.id === x );
+    if(itemArr.length>0)
+    {
+      const item ={
+        id:itemArr[0].id,
+        model:itemArr[0].model,
+        price:itemArr[0].price
+      };
+      temp= [...this.state.list];
+      console.log(temp);
+      temp.push(item);
+      console.log(temp);
+      this.setState({ list : temp });
+      console.log(this.state.list);
+      console.log(item);
+
+    }
+  }
+
+  deleteItem(y)
+  {
+    if(this.state.list.length > 0)
+    {
+      console.log("remove")
+      var temp =[...this.state.list];
+      const updatedList = temp.filter(mobile => mobile.id !== y);
+      this.setState( {list: updatedList} );
+    }
+  }
+
+
+
+
+
+
   render()
   {
     return(
@@ -21,6 +91,14 @@ class App extends React.Component{
                 <div className="caption">
                 <p>Model</p>
                 <p>Price</p>
+                <hr/>
+                <button onClick = {() => this.addItem(1)}>
+                Compare
+                </button>
+                <hr/>
+                <button onClick ={() => this.deleteItem(1)}>
+                Remove
+                </button>
                 </div>
             </div>
 
@@ -29,6 +107,14 @@ class App extends React.Component{
             <div className="caption">
             <p>Model</p>
             <p>Price</p>
+            <hr/>
+            <button onClick = {() => this.addItem(2)}>
+            Compare
+            </button>
+            <hr/>
+            <button onClick ={() => this.deleteItem(2)}>
+            Remove
+            </button>
             </div>
           </div>
 
@@ -37,6 +123,14 @@ class App extends React.Component{
             <div className="caption">
             <p>Model</p>
             <p>Price</p>
+            <hr/>
+            <button onClick = {() => this.addItem(3)}>
+            Compare
+            </button>
+            <hr/>
+            <button onClick ={() => this.deleteItem(3)}>
+            Remove
+            </button>
             </div>
           </div>
 
@@ -45,10 +139,28 @@ class App extends React.Component{
             <div className="caption">
             <p>Model</p>
             <p>Price</p>
+            <hr/>
+            <button onClick = {() => this.addItem(4)}>
+            Compare
+            </button>
+            <hr/>
+            <button onClick ={() => this.deleteItem(4)}>
+            Remove
+            </button>
             </div>
           </div>
         </div>
+          <ul>
+            {this.state.list.map(phone =>{
+              return (<li key={phone.id}>
+                <p>{phone.model}</p>
+                <p>{phone .price}</p>
+                </li>);
+            }
+            )}
+          </ul>
         </div>
+
     )
   }
 
